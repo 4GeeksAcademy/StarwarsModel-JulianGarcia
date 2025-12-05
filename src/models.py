a789ec26415833b1d1462c8e24b7bdade452be6a
juliangarciag05 <juliangarciag05@gmail.com>
2025-12-05 21:44:58 +0000

    finalice tablas

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -17,3 +17,53 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    population: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "climate": self.climate
+
+        }
+    
+
+class People(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    BirthYear: Mapped[int] = mapped_column(String(120), nullable=False)
+    Height: Mapped[str] = mapped_column(String(120), nullable=False)
+    EyeColor: Mapped[str] = mapped_column(String(120), nullable=False)
+    Gender: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "BirthYear": self.BirthYear,
+            "Height": self.Height,
+            "EyeColor": self.EyeColor,
+            "Gender": self.Gender,
+            
+
+        }
+    
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_user: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+    id_people: Mapped[int] = mapped_column(ForeignKey("people.id"), primary_key=True)
+    id_planets: Mapped[int] = mapped_column(ForeignKey("planets.id"), primary_key=True)
+   
+
+
+    
+    
+
